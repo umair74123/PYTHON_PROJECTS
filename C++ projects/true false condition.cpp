b6db7964db7d18eb71d225a37c6cc36a3cbbaf97
umair74123 <114292490+umair74123@users.noreply.github.com>
2022-11-29 19:24:47 +0500

    Delete C++ projects directory

--- C++ projects/true false condition.cpp
+++
@@ -1,10 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	int a, b, X = 5, Y = 10;
-	a = X> Y;	b = Y <X;
-	cout << "True is " << a<< endl;	
-	cout << "False is " << b << endl;
-	system("pause");
-}
